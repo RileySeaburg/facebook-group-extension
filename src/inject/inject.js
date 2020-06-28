5a3893bd8bbfa2d144095b9809dae806cc98f639
Riley Seaburg <riley@rds.marketing>
2020-06-27 20:11:33 -0400

    made slight changes

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -5,17 +5,33 @@ chrome.extension.sendMessage({}, function (response) {
 
       // ----------------------------------------------------------
       // This part of the script triggers when page is done loading
-
-      const request = document.querySelectorAll(
-        "#member_requests_pagelet > div > div > div > div:nth-child(2) > div > div:nth-child(2) > ul > li"
-      );
-      for (let i = 0; i < request.length; i++) {
-        const requestDetails = document.querySelector(
-          "#member_requests_pagelet > div > div > div > div:nth-child(2) > div > div:nth-child(2) > ul > li > div > div:nth-child(2) > div > div > div:nth-child(5)"
-        ).innerText;
-
-        console.log(requestDetails);
+      const numberOfRequests = document
+        .querySelector(
+          `#u_0_1m, #u_0_1n > div > div:nth-child(2) > div > div:nth-child(1) > div > div._ohe.lfloat > div > span`
+        )
+        .innerText.replace(/[^0-9\.]+/g, "")
+        .trim();
+      for (let i = 0; i < numberOfRequests; i + 10) {
+        window.scrollTo(0, document.body.scrollHeight);
       }
+      const requests = document.querySelectorAll(
+        "ul.uiList._4kg._4kt._6-h._6-j > li"
+      );
+      requests.forEach(function (request) {
+        // const parser = new DOMParser();
+        // const parsedRequest = parser.parseFromString(
+        //   request.innerHTML,
+        //   "text/html"
+        // );
+        // console.dir(parsedRequest.all[35].innerHTML);
+        const answers =
+          request.children[0].children[1].children[0].children[0].children[4]
+            .children[0].children;
+        for (let i = 0; i < answers.length; i++) {
+          const answer = answers[i];
+          console.log(answer.innerText);
+        }
+      });
       // ----------------------------------------------------------
     }
   }, 10);
