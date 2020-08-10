1bf54e0ed17cb0475a388da7d7af56048067dd4d
rileyseaburg <riley@rileyseaburg.com>
2020-08-10 15:27:28 -0400

    added console.log to test the output

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,20 +7,22 @@ chrome.extension.sendMessage({}, function (response) {
       );
       requests.forEach(function (request) {
         const answers =
-          request.children[0].children[1].children[0].children[0].children[4]
+          request.children[0].children[1].children[0].children[0].children[3]
             .children[0].children;
         for (let i = 0; i < answers.length; i++) {
           let value = 5;
           const answer = answers[i];
-          chrome.storage.local.set(
-            {
-              key: answer.innerText,
-              timestamp: Date.now(),
-            },
-            function () {
-              console.log("Value Set:" + Date.now());
-            }
-          );
+          // Store answers in local storage
+          // chrome.storage.local.set(
+          //   {
+          //     key: answer.innerText,
+          //     timestamp: Date.now(),
+          //   },
+          //   function () {
+          //     console.log("Value Set:" + Date.now());
+          //   }
+          // );
+          console.log(answer.innerText);
         }
       });
       // ----------------------------------------------------------
